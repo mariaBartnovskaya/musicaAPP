a3386387e4786fa372bb3a5591835d7ca7853024
Maryia Bartnouskaya <maryiabartnouskaya@MacBook-Air-Maryia.local>
2023-06-26 09:29:41 +0200

    can't go to another page

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Registration from './components/signin/registration'
 import SignIn from './components/signin/signin'
 import Favourites from './pages/favourites/favourites'
-
+import ProtectedRoute from './protected-routes'
 import Main from './pages/main/main'
 
 import NotFound from './pages/not-found'
@@ -15,9 +15,9 @@ function AppRoutes({ user, setUser }) {
       <Route path="/signin" element={<SignIn setUser={setUser} user={user} />} />
       <Route path="/favourites" element={<Favourites />} />
       <Route path="/registration" element={<Registration />} />
-     
+      <Route element={<ProtectedRoute isAllowed={Boolean(user)} />}>
         <Route path="/" element={<Main />} />
-     
+        </Route>
 
       <Route path="*" element={<NotFound />} />
     </Routes>
