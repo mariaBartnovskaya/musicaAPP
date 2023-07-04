2cc8cb9cd16a9842a18873e4d002b9915fddb0d4
Maryia Bartnouskaya <maryiabartnouskaya@air-maryia.zte.com.cn>
2023-07-04 19:23:42 +0300

    corrected the mistakes

--- src/pages/favourites/favourites.jsx
+++ src/pages/favourites/favourites.jsx
@@ -7,25 +7,38 @@ import SidebarBlock from '../../components/sidebarBlock/sidebarBlock'
 import Personal from '../../components/personal/personal'
 import Player from '../../components/barPlayer/barPlayer'
 import { useGetAllTracksQuery } from '../../store/services/musicApi'
+import Playlist from '../../components/playlist/playlistcontent'
 
 
 function Favourites() {
   const { data =[]} = useGetAllTracksQuery()
   const userID = Number(localStorage.getItem('user_id'))
 
-  const dataTracks = data.filter((track) => track.stared_user.some((user) => user.id === userID))
-
+  const dataTracks = data.filter(track => track.stared_user.some((user) => user.id === userID))
+const playlists = dataTracks.map((item) => (<Playlist key={item.id}
+  name={item.name}
+  author={item.author}
+  album={item.album}
+  time={item.duration_in_seconds}
+  stared_user={item.stared_user}
+  track_file={item.track_file}
+  id={item.id} />))
   return (
     <Wrapper>
       <main className={s.main}>
         <Sidebar />
-
+        
+        <div className={s.main__centerblock}> 
+        <h1>Мои треки</h1>
+        <ul>{playlists}</ul>
+          </div>
+          
         <div className={s.main__sidebar}>
           <Personal />
           <SidebarBlock />
+         
         </div> 
-        <div> <h1>Мои треки</h1>
-          {dataTracks}</div>
+       
         
         <Player tracks={dataTracks} />
       </main>
